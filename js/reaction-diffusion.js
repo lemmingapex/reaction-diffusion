30124f3a291ac17cb00dd68404849e94b6e36b3e
Scott Wiedemann <lemmingapex@gmail.com>
2017-03-21 13:15:05 -0600

    playing with the idea of another fragment shader to perform coloring.  Need to figure this out.

--- js/reaction-diffusion.js
+++ js/reaction-diffusion.js
@@ -1,7 +1,11 @@
+//_presets = [];
+
 // object where GUI settings are stored
 _settings = {
-	feed: 0.046, // 0.021, 0.030, 0.018, 0.043, 0.011, 0.004
-	kill: 0.059, //0.055, 0.056, 0.052, 0.062, 0.045, 0.036
+	feed: 0.046, // 0.021, 0.030, 0.018, 0.043, 0.011, 0.004, 0.024
+	kill: 0.059, //0.055, 0.056, 0.052, 0.062, 0.045, 0.036, 0.054
+	colorA: "#bbbb99",
+	colorB: "#00bb22",
 	resolution: 512
 };
 
@@ -14,7 +18,7 @@ var _canvas;
 // webgl context.  Let's me do gl stuff.
 var gl;
 // the program to be run on the GPU.  has vertex and fragment shaders.
-var _program;
+var _rd_program;
 
 var _framebuffer0;
 var _framebuffer1;
@@ -37,16 +41,16 @@ function mouseUp(e) {
 
 function render() {
 	// set the resolution
-	var u_resolution = gl.getUniformLocation(_program, "u_resolution");
+	var u_resolution = gl.getUniformLocation(_rd_program, "u_resolution");
 	gl.uniform2f(u_resolution, _canvas.width, _canvas.height);
 
-	var u_feed = gl.getUniformLocation(_program, "u_feed");
+	var u_feed = gl.getUniformLocation(_rd_program, "u_feed");
 	gl.uniform1f(u_feed, _settings.feed);
 
-	var u_kill = gl.getUniformLocation(_program, "u_kill");
+	var u_kill = gl.getUniformLocation(_rd_program, "u_kill");
 	gl.uniform1f(u_kill, _settings.kill);
 
-	var u_mouse = gl.getUniformLocation(_program, "u_mouse");
+	var u_mouse = gl.getUniformLocation(_rd_program, "u_mouse");
 	if(_mouseDown) {
 		console.log("" + _mouseX + " " + _mouseY);
 		gl.uniform2f(u_mouse, _mouseX, _mouseY);
@@ -54,7 +58,7 @@ function render() {
 		gl.uniform2f(u_mouse, -2.0, -2.0);
 	}
 
-	var u_image = gl.getUniformLocation(_program, "u_image");
+	var u_image = gl.getUniformLocation(_rd_program, "u_image");
 
 	// swap the textures
 	if(_swap) {
@@ -126,12 +130,6 @@ function generateTrianglesArrayFromRectangle(x, y, width, height) {
 }
 
 function init() {
-	// settings gui with stuff like 'feed rate' and 'kill rate'
-	var gui = new dat.GUI();
-	gui.add(_settings, "feed", 0, 0.1).name("Feed Rate").listen();
-	gui.add(_settings, "kill", 0, 0.1).name("Kill Rate").listen();
-	//gui.add(_settings, "resolution", 128, 1024).name("Resolution").listen();
-
 	_canvas = document.getElementById("canvas");
 	_canvas.style.left = "0px";
 	_canvas.style.top = "0px";
@@ -158,6 +156,14 @@ function init() {
 		return;
 	}
 
+	// settings gui with stuff like 'feed rate' and 'kill rate'
+	var gui = new dat.GUI();
+	gui.add(_settings, "feed", 0, 0.1).name("Feed Rate").listen();
+	gui.add(_settings, "kill", 0, 0.1).name("Kill Rate").listen();
+	// gui.addColor(_settings, "colorA").name("Color A").listen();
+	// gui.addColor(_settings, "colorB").name("Color B").listen();
+	//gui.add(_settings, "resolution", 128, 1024).name("Resolution").listen();
+
 	// setup GLSL sharders and program
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	gl.shaderSource(vertexShader, document.getElementById("2d-vertex-shader").text);
@@ -166,27 +172,20 @@ function init() {
 	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 	gl.shaderSource(fragmentShader, document.getElementById("2d-fragment-shader").text);
 	gl.compileShader(fragmentShader);
+	//
+	// console.log('Shader compiled successfully: ' + gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS));
+	// console.log('Shader compiler log: ' + gl.getShaderInfoLog(fragmentShader));
 
-	console.log('Shader compiled successfully: ' + gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS));
-	console.log('Shader compiler log: ' + gl.getShaderInfoLog(fragmentShader));
+	// var screenFragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+	// gl.shaderSource(screenFragmentShader, document.getElementById("screen-fragment-shader").text);
+	// gl.compileShader(screenFragmentShader);
 
-	_program = gl.createProgram();
-	gl.attachShader(_program, vertexShader);
-	gl.attachShader(_program, fragmentShader);
-	gl.linkProgram(_program);
-	gl.useProgram(_program);
-
-	// Create a buffer for vertex positions
-	var positionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-	// draw two triangles to cover the image
-	gl.bufferData(gl.ARRAY_BUFFER, generateTrianglesArrayFromRectangle(0.0, 0.0, canvas.width, canvas.height), gl.STATIC_DRAW);
-
-	// provide texture coordinates
-	var texcoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-	// draw two triangles to cover the texture
-	gl.bufferData(gl.ARRAY_BUFFER, generateTrianglesArrayFromRectangle(0.0, 0.0, 1.0, 1.0), gl.STATIC_DRAW);
+	//// RD program ////
+	_rd_program = gl.createProgram();
+	gl.attachShader(_rd_program, vertexShader);
+	gl.attachShader(_rd_program, fragmentShader);
+	gl.linkProgram(_rd_program);
+	gl.useProgram(_rd_program);
 
 	// Create a texture.
 	var texture0 = gl.createTexture();
@@ -221,8 +220,14 @@ function init() {
 	gl.bindFramebuffer(gl.FRAMEBUFFER, _framebuffer1);
 	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture1, 0);
 
+	// Create a buffer for vertex positions
+	var positionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	// draw two triangles to cover the image
+	gl.bufferData(gl.ARRAY_BUFFER, generateTrianglesArrayFromRectangle(0.0, 0.0, canvas.width, canvas.height), gl.STATIC_DRAW);
+
 	// look up where the vertex data needs to go.
-	var a_position = gl.getAttribLocation(_program, "a_position");
+	var a_position = gl.getAttribLocation(_rd_program, "a_position");
 	gl.enableVertexAttribArray(a_position);
 	// Tell a_position how to get data out of positionBuffer (ARRAY_BUFFER)
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -231,13 +236,19 @@ function init() {
 	var normalize = false; // do not normalize
 	var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
 	var offset = 0; // where to start
-	gl.vertexAttribPointer(a_position, size, type, normalize, stride, offset)
+	gl.vertexAttribPointer(a_position, size, type, normalize, stride, offset);
+
+	// provide texture coordinates
+	var texcoordBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+	// draw two triangles to cover the texture
+	gl.bufferData(gl.ARRAY_BUFFER, generateTrianglesArrayFromRectangle(0.0, 0.0, 1.0, 1.0), gl.STATIC_DRAW);
 
 	// Tell a_texCoord how to get data out of texcoordBuffer
-	var a_texCoord = gl.getAttribLocation(_program, "a_texCoord");
+	var a_texCoord = gl.getAttribLocation(_rd_program, "a_texCoord");
 	gl.enableVertexAttribArray(a_texCoord);
 	gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-	gl.vertexAttribPointer(a_texCoord, size, type, normalize, stride, offset)
+	gl.vertexAttribPointer(a_texCoord, size, type, normalize, stride, offset);
 
 	// Tell WebGL how to convert from clip space to pixels
 	gl.viewport(0, 0, _canvas.width, _canvas.height);
